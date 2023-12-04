dd817c5c7c43eb1aade9fd8b6ccb5b5fc116edab
dario <matiasdario75@gmail.com>
2023-12-04 07:49:01 -0300

    feat: dependencies installed and removed and project configured
    - Installed dependencies: fastify, @nestjs/platform-fastify, class-validator and class-transformer.
    - Removed dependencies: @nestjs/platform-express and @types/express.
    - Express replaced by Fastify.
    - Added cors for origin 'http://localhost:3000'.
    - Added ValidationPipe.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
