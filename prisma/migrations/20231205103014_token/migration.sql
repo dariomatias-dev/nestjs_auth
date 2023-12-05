12d5ba9fa07e24b6d4a7780c7dbe558d4b2c45f0
dario <matiasdario75@gmail.com>
2023-12-05 07:32:40 -0300

    feat: token table model created and used
    - Model for the token table, which contains the access_token and the refresh_token created and implemented in the user model.

---
+++ prisma/migrations/20231205103014_token/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "token" (
+    "id" TEXT NOT NULL,
+    "accessToken" TEXT NOT NULL,
+    "refreshToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "token_userId_key" ON "token"("userId");
+
+-- AddForeignKey
+ALTER TABLE "token" ADD CONSTRAINT "token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
